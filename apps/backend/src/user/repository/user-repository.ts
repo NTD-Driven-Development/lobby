fbacbd52c8cff91ec52b61c18d856fefdb922280
GXiang314 <akz232032@gmail.com>
2024-05-24 21:35:55 +0800

    refactor: removed unused user repository method.

--- apps/backend/src/user/repository/user-repository.ts
+++ apps/backend/src/user/repository/user-repository.ts
@@ -3,9 +3,8 @@ import { AbstractRepository, User, UserId } from '@packages/domain'
 export interface UserRepository extends AbstractRepository<User, UserId> {
     existsUserByEmail(email: string): boolean
     findById(id: UserId): Promise<User>
-    findAllById(ids: UserId[]): Promise<User[]>
+    findAll(ids: UserId[]): Promise<User[]>
     findByEmail(email: string): Promise<User>
-    findByIdentity(identity: string): Promise<User>
     save(user: User): Promise<void>
     delete(user: User): Promise<void>
 }
