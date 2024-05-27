05511cf1325a3a07565678ec703f301be4799ef1
GXiang314 <akz232032@gmail.com>
2024-05-27 10:20:52 +0800

    fix: fix leave-room type schema.

--- packages/domain/src/aggregate/room/events/player-left-room.ts
+++ packages/domain/src/aggregate/room/events/player-left-room.ts
@@ -1,6 +1,5 @@
 import { PlayerId, RoomId } from '../entity'
 import { DomainEvent } from '../../../core/entity/domain-event'
-import { LeaveRoomCommandSchema } from '../command'
 import { RoomEvent } from './room-event'
 
 export type PlayerLeftRoomSchema = {
@@ -10,13 +9,13 @@ export type PlayerLeftRoomSchema = {
 
 export class PlayerLeftRoom extends DomainEvent implements RoomEvent {
     constructor(public readonly data: PlayerLeftRoomSchema) {
-        super('player-leaved-room', new Date())
+        super('player-left-room', new Date())
     }
 }
 
 export type PlayerLeftRoomEventSchema = PlayerLeftRoom
 
 export type LeaveRoomEventSchema = {
-    type: 'player-leave-room'
-    data: LeaveRoomCommandSchema & { roomId: RoomId }
+    type: 'leave-room'
+    data: { roomId: RoomId }
 }
