3319f4b3a51a5ff165ae40a4d74e094f4ed71beb
GXiang314 <akz232032@gmail.com>
2024-05-29 06:02:35 +0800

    fix: Auto-add player to socket room on connection if already in a room.

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -12,9 +12,21 @@ import { UserController } from '~/user/adapter/user-controller'
 
 export const UserEventHandlers = async (socket: Server) => {
     const userController = container.resolve(UserController)
-    // 使用者註冊事件
     // auto register user when socket connected
-    await userController.registerUser(socket.auth.user)
+    await userController.registerUser(socket.auth.user).then(() => {
+        console.log('user registered')
+        userController.getMyStatus(socket.auth.user).then((status) => {
+            if (status.data.roomId) {
+                if (!socket.rooms.has(status.data.roomId)) {
+                    socket.join(status.data.roomId)
+                    console.log('join room', status.data.roomId)
+                } else {
+                    console.log('exist room', status.data.roomId)
+                }
+            }
+        })
+    })
+    // 使用者註冊事件
     socket.on('register-user', async () => {
         await userController.registerUser(socket.auth.user)
     })
@@ -22,6 +34,8 @@ export const UserEventHandlers = async (socket: Server) => {
     socket.on('update-user-info', async (event) => {
         await userController.updateUserInfo(event, socket.auth.user)
     })
+    // 取得使用者狀態事件
+
     socket.on('get-my-status', async () => {
         socket.emit('get-my-status-result', await userController.getMyStatus(socket.auth.user))
     })
