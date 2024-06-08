0f528a306dd68b77c75406e4985118c4b1db54f6
Nacke <spexial77941@gmail.com>
2024-06-09 05:14:45 +0800

    implement.

--- apps/frontend/pages/register.vue
+++ apps/frontend/pages/register.vue
@@ -1,17 +1,17 @@
 <template>
     <NuxtLayout name="dashboard">
-        <div class="h-full p-6">
+        <div class="h-full p-3 md:p-6">
             <div class="flex w-full gap-2 md:gap-4 overflow-hidden">
                 <div class="flex flex-1 flex-col overflow-hidden">
                     <div class="flex items-center text-xs gap-0.5 p-1.5 md:text-sm md:p-2">
-                        <span class="text-gray-500">名稱</span>
+                        <span class="text-gray-500">遊戲名稱</span>
                         <span class="text-red-500">*</span>
                     </div>
                     <InputText v-model="state.form.name" class="text-sm grow basis-0 min-w-0 md:text-base"/>
                 </div>
                 <div class="flex flex-1 flex-col overflow-hidden">
                     <div class="flex items-center text-xs gap-0.5 p-1.5 md:text-sm md:p-2">
-                        <span class="text-gray-500">敘述</span>
+                        <span class="text-gray-500">遊戲敘述</span>
                         <span class="text-red-500">*</span>
                     </div>
                     <InputText v-model="state.form.description" class="text-sm grow basis-0 min-w-0 md:text-base"/>
@@ -20,7 +20,7 @@
             <div class="flex w-full gap-2 md:gap-4 overflow-hidden">
                 <div class="flex flex-1 flex-col overflow-hidden">
                     <div class="flex items-center text-xs gap-0.5 p-1.5 md:text-sm md:p-2">
-                        <span class="text-gray-500">規則</span>
+                        <span class="text-gray-500">遊戲規則</span>
                         <span class="text-red-500">*</span>
                     </div>
                     <InputText v-model="state.form.rule" class="text-sm grow basis-0 min-w-0 md:text-base"/>
@@ -66,7 +66,7 @@
                 </div>
             </div>
             <div class="flex pt-4 md:pt-6">
-                <Button label="送出" class="w-full text-sm md:text-base" @click="submit"/>
+                <Button label="送出" class="w-full text-sm" @click="submit"/>
             </div>
             <Toast position="bottom-center"></Toast>
         </div>
@@ -75,19 +75,22 @@
 
 <script setup lang="ts">
     const appStore = useAppStore();
-    const { waitSocketConnected } = appStore;
-    const { lobbySocket } = storeToRefs(appStore);
+    const { app, waitSocketConnected } = appStore;
     const registerStore = useRegisterStore();
-    const { submit } = registerStore;
-    const { state } = storeToRefs(registerStore);
+    const { state, submit } = registerStore;
     const toast = useToast();
 
     onMounted(async () => {
         await waitSocketConnected();
 
-        lobbySocket.value?.on('game-registered', (event) => {
-            console.log(event);
-            toast.add({ severity: 'info', summary: '處理結果', detail: '遊戲註冊成功', life: 1500 });
-        });
+        app.lobbySocket?.on('game-registered', gameRegisterListner);
     });
+
+    onUnmounted(() => {
+        app.lobbySocket?.off('game-registered', gameRegisterListner);
+    });
+
+    const gameRegisterListner = () => {
+        toast.add({ severity: 'info', summary: '處理結果', detail: '遊戲註冊成功', life: 1500 });
+    }
 </script>
