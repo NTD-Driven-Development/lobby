0914fc7b645af82c057bd466c2d0a55294ba98b0
Nacke <spexial77941@gmail.com>
2024-06-11 07:55:08 +0800

    update.

--- apps/frontend/pages/index.vue
+++ apps/frontend/pages/index.vue
@@ -1,40 +1,36 @@
 <template>
     <NuxtLayout name="dashboard">
         <div class="flex flex-col h-full gap-3 p-3 md:p-6">
-            <div class="flex shrink-0 gap-2 overflow-auto">
-                <div class="flex w-full gap-2" v-if="state.games">
-                    <div v-for="game in state.games" class="relative flex items-center justify-center min-w-[120px] max-w-[120px] min-h-[48px] max-h-[48px] text-sm border rounded cursor-pointer whitespace-prewrap"
-                    :class="{ ' bg-gray-50/30 border-gray-400': game.id == state.currentGame?.id, 'bg-white': game.id != state.currentGame?.id }"
-                    @click="state.currentGame = game">
-                        <span>{{ game.name }}</span>
-                        <div class="absolute"></div>
-                    </div>
+            <div class="flex shrink-0 gap-2 overflow-auto" v-if="state.games">
+                <div v-for="game in state.games" class="relative flex items-center justify-center min-w-[120px] max-w-[120px] min-h-[48px] max-h-[48px] text-sm border rounded cursor-pointer whitespace-prewrap"
+                :class="{ ' bg-gray-50/30 border-gray-400': game.id == state.currentGame?.id, 'bg-white': game.id != state.currentGame?.id }"
+                @click="state.currentGame = game">
+                    <span>{{ game.name }}</span>
+                    <div class="absolute"></div>
                 </div>
             </div>
             <div class="flex shrink-0 justify-around gap-2">
                 <InputText placeholder="搜尋房間" v-model="state.search" class="w-full text-sm"></InputText>
                 <Button label="建立房間" @click="state.createRoomDialog.show = true" class="shrink-0 text-sm"></Button>
             </div>
-            <div class="grid flex-1 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] auto-rows-min gap-2">
-                <div v-if="state.rooms">
-                    <div v-for="room in state.rooms" class="flex flex-col border p-3 gap-3 cursor-pointer rounded-lg text-sm"
-                    @click="onRoomClick(room)">
-                        <div class="flex flex-col gap-1.5">
+            <div class="grid flex-1 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] auto-rows-min gap-2" v-if="state.rooms">
+                <div v-for="room in state.rooms" class="flex flex-col border p-3 gap-3 cursor-pointer rounded-lg text-sm"
+                @click="onRoomClick(room)">
+                    <div class="flex flex-col gap-1.5">
+                        <div>
+                            <span>房間名稱：</span>
+                            <span>{{ room.name }}</span>
+                        </div>
+                        <div>
+                            <span>遊戲名稱：</span>
+                            <span>{{ room.game.name }}</span>
+                        </div>
+                        <div class="flex justify-between">
                             <div>
-                                <span>房間名稱：</span>
-                                <span>{{ room.name }}</span>
+                                <span>人數：</span>
+                                <span>{{ `${room.currentPlayers}/${room.maxPlayers}` }}</span>
                             </div>
-                            <div>
-                                <span>遊戲名稱：</span>
-                                <span>{{ room.game.name }}</span>
-                            </div>
-                            <div class="flex justify-between">
-                                <div>
-                                    <span>人數：</span>
-                                    <span>{{ `${room.currentPlayers}/${room.maxPlayers}` }}</span>
-                                </div>
-                                <div class="text-red-500" v-if="room.isLocked">需要密碼</div>
-                            </div>
+                            <div class="text-red-500" v-if="room.isLocked">需要密碼</div>
                         </div>
                     </div>
                 </div>
