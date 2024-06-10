155d9da38a49c60f34a4d37024e4f924564d9c2e
Nacke <spexial77941@gmail.com>
2024-06-11 06:53:30 +0800

    fix bug.

--- apps/frontend/pages/room.vue
+++ apps/frontend/pages/room.vue
@@ -6,26 +6,28 @@
                 <span class="p-text-secondary block">遊戲名稱：{{ state.room?.game.name }}</span>
                 <span class="p-text-secondary block">當前人數：{{ state.room?.players?.length }}</span>
             </div>
-            <div class="grid flex-1 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] auto-rows-min gap-2" v-if="state.room?.players">
-                <div v-for="player in state.room?.players" class="flex flex-col border p-3 gap-3 rounded-lg">
-                    <div class="flex flex-col gap-1">
-                        <div class="flex items-center justify-between">
-                            <div>
-                                <span v-if="player.id == state.room?.host.id">(房主)</span>
-                                <span>{{ player.name }}</span>
+            <div class="grid flex-1 grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] auto-rows-min gap-2">
+                <template v-if="state.room?.players">
+                    <div v-for="player in state.room?.players" class="flex flex-col border p-3 gap-3 rounded-lg">
+                        <div class="flex flex-col gap-1">
+                            <div class="flex items-center justify-between">
+                                <div>
+                                    <span v-if="player.id == state.room?.host.id">(房主)</span>
+                                    <span>{{ player.name }}</span>
+                                </div>
+                                <div class="flex justify-end">
+                                    <span class="text-sm text-red-500" v-if="player.isReady">{{ '已就緒' }}</span>
+                                    <span class="text-sm text-gray-500" v-else>{{ '尚未準備' }}</span>
+                                </div>
                             </div>
                             <div class="flex justify-end">
-                                <span class="text-sm text-red-500" v-if="player.isReady">{{ '已就緒' }}</span>
-                                <span class="text-sm text-gray-500" v-else>{{ '尚未準備' }}</span>
+                                <div class="text-sm text-red-500 cursor-pointer"
+                                :class="{ 'invisible': app.status?.id != state.room?.host.id || player.id == state.room?.host.id }"
+                                @click="kick(player.id)">踢出</div>
                             </div>
                         </div>
-                        <div class="flex justify-end">
-                            <div class="text-sm text-red-500 cursor-pointer"
-                            :class="{ 'invisible': app.status?.id != state.room?.host.id || player.id == state.room?.host.id }"
-                            @click="kick(player.id)">踢出</div>
-                        </div>
                     </div>
-                </div>
+                </template>
             </div>
             <div class="flex pt-4 md:pt-6 gap-2">
                 <Button label="退出房間" severity="danger" class="w-full text-sm md:text-base" @click="leave"/>
